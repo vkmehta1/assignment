dc10e1768b3858250ea440052e4fc9c5db5df313
VK Mehta <vikasiamrgroup@gmail.com>
2018-06-28 12:21:04 +0530

    First Assignment

---
+++ Untitled-1.php
@@ -0,0 +1,17 @@
+<?php 
+function tringle($n)
+{
+$k=2*$n-2;
+for($i=0;$i<$n;$i++)
+{
+for($j=0;$j<$k;$j++)
+echo" ";
+$k=$K-1;
+for($j=0;$j<=$i;$j++)
+{
+echo"*";
+}
+echo"\n";
+}
+}tringle($n);
+?>
---
+++ vkmehta11.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<style type="text/css">
+.header{
+	width:100%;
+	height:100px;
+
+	background-image:url(2.JPG);
+	background-repeat:no-repeat;
+	background-size:200px * 900px;
+	float:left;
+	text-align:center;
+	color:#09F;
+		}
+.footer{
+	width:100%;
+	height:200px;
+
+	background-color:#FFFF00;
+	float:left;
+	text-align:center;
+	color:#09F;
+		}		
+.menu{
+	width:100%;
+	height:30px;
+	
+	background-color:green;
+	float:left;
+	text-align:center;
+	color: #FFF;
+	}
+	.left content{
+	width:32%;
+	height:250px;
+	
+	background-color: #F90;
+	float:left;
+	text-align:center;
+	color: #FFF;
+		}
+	.rightcontent{
+	width:32%;
+	height:250px;
+	
+	background-color: #F90;
+	float:left;
+	text-align:center;
+	color: #FFF;
+		}
+	.contact form{
+	width:100%;
+	height:100px;
+	background-color:green;
+	float:left;
+	text-align:center;
+	color: #FFF;
+		}
+</style>
+</head>
+<body>
+<div class="header">
+Site's Header 
+</div>
+<div class="menu">
+Site's Menu
+</div>
+<div class="contact form">
+Site's contact form
+</div>
+<div class="leftcontent">
+<table width="100%">
+</table>
+</div>
+<div class="footer">
+</div>
+
+</body>
+</html>
